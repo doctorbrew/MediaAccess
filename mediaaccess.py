2460ebdd3f12492138c3090eb5017a6437b85be4
Connor O'Reilly <40921747+doctorbrew@users.noreply.github.com>
2023-09-30 21:12:02 +0200

    Fix 'q' to exit

--- mediaaccess.py
+++ mediaaccess.py
@@ -1,25 +1,17 @@
-import os
 import subprocess
 import time
+import sys
+import tty
 
 _cachedOutput = "NULL"
+lastReturnCode = 0
 songFile = "/tmp/currentsong.txt"
 exe = "playerctl"
 delay = 2
 print("Started!  Exit the application using 'q'")
 
 
-def get_error(val):
-    if val == "NULL":
-        return 1
-    elif val == "No players found":
-        return 2
-    else:
-        return 0
-
-
 def set_file(file_name, contents):
-    # print("Setting file: " + contents)
     file = open(file_name, "w")
     file.write(contents)
 
@@ -27,36 +19,45 @@ def set_file(file_name, contents):
 def get_status():
     proc = subprocess.run(["playerctl", "-p", "spotify", "status"], shell=False, capture_output=True)
     output = proc.stdout.decode("utf-8").strip('\n')
-    if proc.returncode != 0:
-        print("[playerctl] Error: " + proc.stderr.decode("utf-8").strip('\n') + " Code: " + str(proc.returncode))
+    global lastReturnCode
+    lastReturnCode = proc.returncode
+    if lastReturnCode != 0:
+        print("[playerctl] Error: " + proc.stderr.decode("utf-8").strip('\n') + " Code: " + str(lastReturnCode))
         return proc.stderr.decode("utf-8").strip('\n')
     return output
 
 
 def get_metadata(name):
-    # print(os.system("playerctl -p spotify metadata title"))
     proc = subprocess.run(["playerctl", "-p", "spotify", "metadata", name], shell=False, capture_output=True)
     output = proc.stdout.decode("utf-8").strip('\n')
-    if proc.returncode != 0:
-        print("[playerctl] Error: " + proc.stderr.decode("utf-8").strip('\n') + " Code: " + str(proc.returncode))
+    global lastReturnCode
+    lastReturnCode = proc.returncode
+    if lastReturnCode != 0:
+        print("[playerctl] Error: " + proc.stderr.decode("utf-8").strip('\n') + " Code: " + str(lastReturnCode))
         return proc.stderr.decode("utf-8").strip('\n')
-    # print(output)
     return output
-    # return ""
 
 
+tty.setcbreak(sys.stdin)
 while True:
     status = get_status()
     time.sleep(delay)
+    if sys.stdin.read(1)[0] == 'q':
+        # os.remove(songFile)
+        # tty.setraw(sys.stdin)
+        exit(0)
+    if lastReturnCode != 0:
+        set_file(songFile, "Error getting info.")
+        continue
     if status == "Paused":
-        set_file(songFile, "Paused")
+        set_file(songFile, "No media")
         continue
     artist = get_metadata("artist")
-    if get_error(artist) != 0:
+    if lastReturnCode != 0:
         set_file(songFile, "Error getting info.")
         continue
     title = get_metadata("title")
-    if get_error(title) != 0:
+    if lastReturnCode != 0:
         set_file(songFile, "Error getting info.")
         continue
-    set_file(songFile, artist + " - " + title)
+    set_file(songFile, "[" + artist + "] " + title)
